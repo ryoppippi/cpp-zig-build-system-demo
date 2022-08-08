758efc7133327eb9bba672362907951839edd2cc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-08-08 12:57:20 +0100

    add run step

--- build.zig
+++ build.zig
@@ -34,6 +34,14 @@ pub fn build(b: *Builder) void {
 
     exe.linkLibCpp();
     exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
 
 fn ensureSubmodules(allocator: std.mem.Allocator) !void {
