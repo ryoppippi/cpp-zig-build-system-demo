32423c7c9618b4430ae9052ffac67206658e6452
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-09-28 12:21:29 +0100

    change build command

--- build.zig
+++ build.zig
@@ -5,11 +5,11 @@ pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    ensureSubmodules(b.allocator) catch |err| @panic(@errorName(err));
-
     const exe = b.addExecutable("main", null);
     exe.setTarget(target);
     exe.setBuildMode(mode);
+
+    exe.step.dependOn(&b.addSystemCommand(&.{ "git", "submodule", "update", "--init", "--recursive" }).step);
 
     exe.addCSourceFile("src/main.cpp", &[_][]const u8{});
     exe.addIncludePath("third_party/eigen");
@@ -46,17 +46,6 @@ pub fn build(b: *Builder) void {
     }
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-}
-
-fn ensureSubmodules(allocator: std.mem.Allocator) !void {
-    if (std.process.getEnvVarOwned(allocator, "NO_ENSURE_SUBMODULES")) |no_ensure_submodules| {
-        if (std.mem.eql(u8, no_ensure_submodules, "true")) return;
-    } else |_| {}
-    var child = std.ChildProcess.init(&.{ "git", "submodule", "update", "--init", "--recursive" }, allocator);
-    child.cwd = (comptime thisDir());
-    child.stderr = std.io.getStdErr();
-    child.stdout = std.io.getStdOut();
-    _ = try child.spawnAndWait();
 }
 
 fn thisDir() []const u8 {
