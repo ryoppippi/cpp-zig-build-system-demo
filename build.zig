d77f8a93bce91019ed155241b9655d90ea7ad053
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-17 13:53:16 +0100

    feat(build): update build.zig for Zig 0.12.0 compatibility
    This commit updates the build.zig file to be compatible with Zig 0.12.0.
    The changes include updating the Builder to Build, adding new build
    options, and updating the way executable is added and configured. The
    commit also updates devbox.json and devbox.lock to include new
    dependencies: pkg-config and openblas.

--- build.zig
+++ build.zig
@@ -1,53 +1,53 @@
 const std = @import("std");
-const Builder = std.build.Builder;
+const Build = std.Build;
+const Step = Build.Step;
+const Compile = Step.Compile;
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("main", null);
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    const exe = b.addExecutable(.{
+        .name = "main",
+        .target = target,
+        .optimize = optimize,
+    });
 
     exe.step.dependOn(&b.addSystemCommand(&.{ "git", "submodule", "update", "--init", "--recursive" }).step);
 
-    exe.addCSourceFile("src/main.cpp", &[_][]const u8{});
-    exe.addIncludePath("third_party/eigen");
-    exe.addIncludePath("third_party/spectra/include");
+    exe.addCSourceFile(.{ .file = .{ .path = "src/main.cpp" }, .flags = &.{} });
+    exe.addIncludePath(.{ .path = "third_party/eigen" });
+    exe.addIncludePath(.{ .path = "third_party/spectra/include" });
 
     exe.defineCMacro("EIGEN_FAST_MATH", "1");
     exe.defineCMacro("THREAD_SAFE", "");
     exe.linkSystemLibrary("m");
 
-    if (target.isNative()) {
+    if (target.query.isNative()) {
         exe.defineCMacro("EIGEN_USE_BLAS", "");
         exe.linkSystemLibrary("blas");
         exe.linkSystemLibrary("omp");
-        if (target.isDarwin()) {
-            exe.linkFramework("Accelerate");
-            exe.addIncludePath("/opt/homebrew/include");
-            exe.addIncludePath("/usr/local/include");
-            exe.addLibraryPath("/opt/homebrew/lib");
-            exe.addLibraryPath("/usr/local/lib");
+        if (target.result.isDarwin()) {
+            // exe.linkFramework("Accelerate");
+            exe.addIncludePath(.{ .path = "/opt/homebrew/include" });
+            exe.addLibraryPath(.{ .path = "/usr/local/include" });
+            exe.addLibraryPath(.{ .path = "/opt/homebrew/lib" });
+            exe.addLibraryPath(.{ .path = "/usr/local/lib" });
         }
     }
 
-    if (b.is_release) {
+    if (optimize != .Debug) {
         exe.defineCMacro("EIGEN_NO_DEBUG", "");
     }
 
     exe.linkLibCpp();
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-}
-
-fn thisDir() []const u8 {
-    return std.fs.path.dirname(@src().file) orelse ".";
 }
